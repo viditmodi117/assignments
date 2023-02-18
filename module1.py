498968e261c0a0ae3f95e6631413da9ca28bcd26
viditmodi117 <112745985+viditmodi117@users.noreply.github.com>
2023-02-18 19:16:28 +0530

    module1 submitted

---
+++ module1.py
@@ -0,0 +1,41 @@
+# What is Software?
+#ANS---->
+'''Software is a set of instructions, data or programs used   to operate
+computers and execute specific tasks. It is the opposite  of    hardware, 
+which describes the physical aspects of a computer. Software is a generic
+term used to refer to applications, scripts and programs that run on a device.'''
+
+# What are the types of Applications?
+#ANS.---->
+'''An application, also referred to as an application program or 
+application software, is a computer software package that performs 
+a specific function directly for an end user or, in some cases, for
+another application. An application can be self-contained or a group
+of programs. The program is a set of operations that runs the application for the user.
+1.Word processors
+2.Database programs
+3.Web browsers
+4.Deployment tools
+5.Image editors
+6.Communication platforms
+'''
+
+#What is programing?
+#ANS---->
+'''Programming is the process of creating a set of instructions 
+that tell a computer how to perform a task. Programming can be 
+done using a variety of computer programming languages, such as
+JavaScript, Python, and C++'''
+
+#What is Python?
+#ANS---->
+'''Python is an interpreted,   object-oriented, high-level 
+programming language with dynamic semantics  developed  by
+Guido van Rossum. It    was originally    released in 1991. 
+Designed to be easy as    well as fun,    the name "Python" 
+is a nod to the British comedy    group    Monty    Python. 
+Python has a reputation as a   beginner-friendly   language, 
+replacing Java as the most widely used introductory language 
+because it handles much of the complexity for the user, allowing 
+beginners to focus on fully grasping programming concepts rather 
+than minute details.'''
